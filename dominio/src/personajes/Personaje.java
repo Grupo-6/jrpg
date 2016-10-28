fa0e17f158d231c63420ba3a3c8b30502c3b493b
RodTerra <rodxte@gmail.com>
2016-10-28 18:33:49 -0300

    agregado experiancia y nivels. ahora sube de nivel y aumenta XP

--- dominio/src/personajes/Personaje.java
+++ dominio/src/personajes/Personaje.java
@@ -1,9 +1,12 @@
 package personajes;
 
+import java.util.Scanner;
+
 public abstract class Personaje implements Atacable {
 
 	protected int energia = 100;
 	protected int salud = 100;
+	protected int lvl=1;
 
 	protected int energiaMax = 100;
 	protected int saludMax = 100;
@@ -11,6 +14,9 @@ public abstract class Personaje implements Atacable {
 	protected int ataque;
 	protected int defensa;
 	protected int velocidad;
+	
+	protected int experiencia=0;
+	protected int expMax=100;
 
 	protected Casta casta=null;
 	protected final int guerrero = 0;
@@ -23,7 +29,44 @@ public abstract class Personaje implements Atacable {
 			despuesDeAtacar();
 		}
 	}
-
+	public final void lvlUp(){
+		
+		if(this.lvl<10){
+			this.lvl+=1;
+			this.expMax+=100;
+			this.experiencia=0;
+			Scanner entrada = new Scanner (System.in);
+			int option=0;
+			for(int i =0;i<=5;i++){
+				while(option<1 || option >3)
+					option=entrada.nextInt();
+				switch(option){
+					case 1:
+						this.ataque+=1;
+						break;
+					case 2:
+						this.defensa+=1;
+						break;
+					case 3: 
+						this.velocidad+=1;
+						break;
+			}
+				option=0;
+		}
+			entrada.close();
+		}
+	}
+	public final void aumentarXP(int lvlEnemy){
+		if(this.lvl <10){
+			this.experiencia+=(lvlEnemy)*20;
+			int xp=this.experiencia-this.expMax;
+			while(xp>=0){
+				this.lvlUp();
+				this.experiencia+=xp;
+				xp=this.experiencia-this.expMax;
+			}
+		}
+	}
 	@Override
 	public void serAtacado(int danio) {
 		danio = this.obtenerPuntosDeDefensa() - danio;
